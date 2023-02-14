585a0c671a799876093169aad2d30162619df7d2
martingaw11 <martingaw11@gmail.com>
2023-02-14 16:35:30 -0600

    cleaning up for submission

--- 1.4.0 Observing Elapsed Time/src/StopwatchClient.java
+++ 1.4.0 Observing Elapsed Time/src/StopwatchClient.java
@@ -1,7 +1,15 @@
+/*
+ * Martin Gawron
+ * 02/14/2023
+ * CSC-280-350
+ * Assignment: 1.4.0 A Observing Elapsed Time
+ */
+
+// imported Stopwatch from algs4 rather copying Stopwatch data type into seperate class
 import edu.princeton.cs.algs4.Stopwatch;
 import edu.princeton.cs.algs4.StdOut;
 
-public class ElapsedTime {
+public class StopwatchClient {
     /*
      * This is the implementation copied from the PrimeNumbers.html file given to
      * us for the assignment. Slight adjustment was made where the parameter of 
@@ -38,7 +46,11 @@ public class ElapsedTime {
         return count;
     }
 
-    
+    /*
+     * This is the implementation copied from the EfficientPrimeNumbers.html file given to
+     * us for the assignment. Slight adjustment was made where the parameter of 
+     * upper bounds for the search are passed for a method call.
+     */
     public static int EfficientPrimeNumbers(int n) {
         // Find all prime numbers <= n
 
@@ -81,9 +93,17 @@ public class ElapsedTime {
         return count;
     }
     
-
+    /*
+     * This is the test client of StopwatchClient, the main method is to run
+     * each method for different upper bounds of search for prime numbers,
+     * a stopwatch from Algs4 is used to see how much time it takes for the 
+     * method to return the correct value. The results are printed in a table 
+     * formatted so that the time values can be compared and the time complexity
+     * can be analyzed.
+     */
     public static void main(String[] args) throws Exception {
         int size = 8000000;
+        /* WAS USED TO COMPARE METHOD OUTPUTS
         for (int i = 0; i < 6; i++) {
             int delta = 2000000 * i;
             StdOut.print(PrimeNumbers(size+delta) + "\t");
@@ -93,8 +113,11 @@ public class ElapsedTime {
             int delta = 2000000 * i;
             StdOut.print(EfficientPrimeNumbers(size+delta) + "\t");
         }
-        StdOut.println("\n\n");
+        */
 
+        StdOut.println("\n");
+
+        // formatting table header showing upper bounds of each function call
         StdOut.printf("%30s", " ");
         for (int i = 0; i < 6; i++) {
             int delta = 2000000 * i;
@@ -107,6 +130,9 @@ public class ElapsedTime {
             StdOut.print("-");
         }
 
+        // for loop goes through and finds how long it takes for the PrimeNumbers
+        //  method to find the number of prime numbers in the upper bounds passed
+        // and then formatted to show the time(seconds) to the thousandths
         StdOut.println();
         StdOut.printf("%-30s", "Prime Numbers");
         for (int i = 0; i < 6; i++) {
@@ -118,6 +144,9 @@ public class ElapsedTime {
             StdOut.printf("%10s", ((Double)time).toString());
         }
 
+        // same as loop before but this time running upper bounds to the
+        // EfficientPrimeNumbers method to input the time it takes to find the 
+        // number of prime numbers within the argument and formats it into the table
         StdOut.println();
         StdOut.printf("%-30s", "Efficient Prime Numbers");
         for (int i = 0; i < 6; i++) {
@@ -128,5 +157,8 @@ public class ElapsedTime {
             double time = timer.elapsedTime();
             StdOut.printf("%10s", ((Double)time).toString());
         }
+        StdOut.println("\n");
+
+        // As you can see from the output, EfficientPrimeNumbers is considerably faster
     }
 }
