2f2b8cd1174f0b918e2a54a97538206d9a1aa1b0
martingaw11 <martingaw11@gmail.com>
2023-05-05 13:33:48 -0500

    ran text files through graph and printed results, add drawn pictures of graphs

--- 4.1.7 Print a Graph/src/GraphClient.java
+++ 4.1.7 Print a Graph/src/GraphClient.java
@@ -5,10 +5,24 @@
  * 
  */
 
+import java.io.File;
+
 import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
 
 public class GraphClient {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        In tinyGText = new In(new File("src/tinyG.txt"));
+        In graph1Text = new In(new File("src/graph1.txt"));
+        In graph2Text = new In(new File("src/graph2.txt"));
+        
+        Graph tinyGGraph = new Graph(tinyGText);
+        Graph graph1 = new Graph(graph1Text);
+        Graph graph2 = new Graph(graph2Text);
+
+        StdOut.println("tinyG.txt graph\n------------------\n" + tinyGGraph + "\n");
+        StdOut.println("graph1.txt graph\n------------------\n" + graph1 + "\n");
+        StdOut.println("graph2.txt graph\n------------------\n" + graph2 + "\n");
     }
 }
