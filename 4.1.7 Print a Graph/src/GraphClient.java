139aec2f44cf72ec836dbe695ba3e8909eeb29d7
martingaw11 <martingaw11@gmail.com>
2023-05-05 12:17:32 -0500

    init start to 4.1.7 Print a Graph

---
+++ 4.1.7 Print a Graph/src/GraphClient.java
@@ -0,0 +1,14 @@
+/*
+ * Martin Gawron
+ * 05/05/2023
+ * Initial Start to 4.1.7 assignment
+ * 
+ */
+
+import edu.princeton.cs.algs4.Graph;
+
+public class GraphClient {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
